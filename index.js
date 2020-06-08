a0ce3036596f68812728e6b72aa907244761c29d
stephaneleonard <stefleonard97@gmail.com>
2020-06-08 11:37:58 +0200

    corrected link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 // app.use(express.static("/phaser3-parcel-template/dist/"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/phaser3-parcel-template/dist/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", (socket) => {
