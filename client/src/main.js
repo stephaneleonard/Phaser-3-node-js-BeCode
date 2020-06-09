a1f9add7ceaf9bc8ccaa7afa32ece003d239a405
stephaneleonard <stefleonard97@gmail.com>
2020-06-09 15:16:36 +0200

    added party_ready event

--- client/src/main.js
+++ client/src/main.js
@@ -2,26 +2,26 @@ import Phaser from "phaser";
 
 import HelloWorldScene from "./scenes/HelloWorldScene";
 import GameScene from "./scenes/GameScene";
-import smashGameScene from "./scenes/smashGameScene";
+// import smashGameScene from "./scenes/smashGameScene";
 export const socket = io();
 
 document.addEventListener("keypress", (e) => {
   if (e.code == "KeyW") {
-    socket.emit("position", [100,100]);
+    socket.emit("position", [100, 100]);
   }
   if (e.code == "KeyS") {
-    socket.emit("position", [100,100]);
+    socket.emit("position", [100, 100]);
   }
   if (e.code == "KeyA") {
-    socket.emit("position", [100,100]);
+    socket.emit("position", [100, 100]);
   }
   if (e.code == "KeyD") {
-    socket.emit("position", [100,100]);
+    socket.emit("position", [100, 100]);
   }
 });
 
 socket.on("playerPosition", (obj) => {
-  console.log(obj);
+  // console.log(obj);
 });
 
 socket.on("socketID", (obj) => {
@@ -33,10 +33,12 @@ const config = {
   width: 800,
   height: 500,
   physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-    },
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
+      }
   },
-  scene: [smashGameScene],
+  scene: [GameScene, HelloWorldScene],
 };
+const game = new Phaser.Game(config);
