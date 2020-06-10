decd66f3b087274b262bb5344b6956697ec45b93
Meunier Emmanuel <Emmanuel.meunier.dev@gmail.com>
2020-06-10 11:23:36 +0200

    merge

--- models/Player.js
+++ models/Player.js
@@ -3,54 +3,34 @@ module.exports = class Player {
     this.socketID = socket;
     this.positionX = 100;
     this.positionY = 100;
-<<<<<<< HEAD
-=======
     //player damage
-    this.damage = 5; 
->>>>>>> master
+    this.damage = 5;
   }
 
-  move(array){
+  move(array) {
 
     this.positionX = array[0];
     this.positionY = array[1];
   }
 };
 
-<<<<<<< HEAD
-  //     if(key == 1){
-  //         this.positionX += 2;
-  //     }
-  //     else if(key == 2){
-  //       this.positionX -= 2;
-  //     }
-  //     else if(key == 3){
-  //       this.positionY += 2;
-  //     }
-  //     else if(key == 4){
-  //       this.positionY -= 2;
-  //     }
-  // }
+/*
+ * update player damage counter
+ * input: int
+ * output: none
+ */
+setPlayerDamage(damage) {
+  //add damage taken to the damage property
+  this.damage += damage;
+}
 
-=======
-  /*
-   * update player damage counter
-   * input: int
-   * output: none
-   */
-  setPlayerDamage(damage) {
-    //add damage taken to the damage property
-    this.damage += damage;
-  }
-
-  /*
-   * send player damage counter
-   * input: none
-   * output: int
-   */
-  getPlayerDamage() {
-    // send player damage
-    return this.damage; 
-  }
+/*
+ * send player damage counter
+ * input: none
+ * output: int
+ */
+getPlayerDamage() {
+  // send player damage
+  return this.damage;
+}
 };
->>>>>>> master
