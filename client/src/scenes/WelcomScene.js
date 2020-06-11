636fbd0d6e396d489687fa7d094de1abe111f43f
Kevin Pirnay <kevinpirnay@MacBook-Air-de-Kevin.local>
2020-06-11 13:56:39 +0200

    schema logique

--- client/src/scenes/WelcomScene.js
+++ client/src/scenes/WelcomScene.js
@@ -7,20 +7,43 @@ export default class GameScene extends Phaser.Scene {
   }
 
 
-
-
-  create() {
+  create(){
     /*
      * when party is ready launch next scene and pass it the array with the other players object
      *
      */
+    //let buttonJoin('button');
 
-    
-     
+
     socket.emit('createRoom','data');
-    socket.on('update',(data)=>
+
+
+    socket.on('update',(data,idRoom = 111)=>
         {
-            console.log(data); 
+            console.log('update',data);
+            
+            socket.emit('join',idRoom);
+        }
+    );
+
+
+    socket.on('joinEvent',(idRoom)=> //boutton
+        {
+            socket.emit('join',idRoom);
+        }
+    );
+
+
+    socket.on('playerJoinRoom',(data)=>
+        {
+            console.log('Player has join room',data); 
+        }
+    );
+
+
+    socket.on("party_ready",()=>
+        {
+            console.log('finish');
         }
     );
   }
--- index.js
+++ index.js
@@ -28,9 +28,9 @@ io.on("connection", (socket) => {
   console.log(playerArray);
   if (Object.keys(playerArray).length >= 2) {
     io.emit("party_ready", playerArray);
-    console.log("test");
+    //console.log("test");
   }
-  console.log(`player ${id} connected`);
+  //console.log(`player ${id} connected`);
 
   //get the new position from a client
   socket.on("position", (obj) => {
@@ -45,14 +45,39 @@ io.on("connection", (socket) => {
       player.positionY,
     ]);
 
-    //test room
     
-  
-  
+  //test room
+    socket.on("createRoom", (data) => {
+      console.log("event createRoom");
+
+      //data.name
+      const room = new Room(roomCount, data);
+      roomCount++;
+      rooms.push(room);
+      console.log('rooms' , rooms);
+
+      io.sockets.emit("update", rooms);
+    });
+
+    //test room
 
   socket.on('join',(data)=>
     {
-      //rooms.filter(room =>{}) //????? roomCount++
+      const select = rooms.filter(room => data.id === room.id);
+      Object.keys(playerArray).push(player);
+      select.length ++;
+      
+      console.log(select);
+      
+
+      socket.emit('playerJoinRoom',rooms)
+
+
+      if (Object.keys(playerArray).length >= 2) {
+        io.emit("party_ready", playerArray);
+
+        console.log("game");
+      }
     }
   );
     
@@ -61,28 +86,17 @@ io.on("connection", (socket) => {
   socket.on("hit", (dir) => {
     // check if hit
     player.hasHitOtherPlayer(playerArray, dir, io);
-    console.log(playerArray);
+    //console.log(playerArray);
     // update damage count on players hit and send this value to those player
   });
 
   socket.on("disconnect", () => {
-    console.log(`player ${id} disconected`);
+    //console.log(`player ${id} disconected`);
     // delete this player from the array
     delete playerArray[id];
   });
 
-  //test room
-  socket.on("createRoom", (data) => {
-    console.log("event createRoom");
-
-    //data.name
-    const room = new Room(roomCount, data);
-    roomCount++;
-    rooms.push(room);
-    console.log('rooms' , rooms);
-
-    io.sockets.emit("update", rooms);
-  });
+  
 });
 
 const PORT = process.env.PORT || 3000;
